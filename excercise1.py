66f003f6249fbd2d4017477b4b0f23f85bfefade
Robert <110719811+RobertCwenar@users.noreply.github.com>
2025-11-17 10:30:40 +0000

    install python

--- excercise1.py
+++ excercise1.py
@@ -2,10 +2,11 @@
 szukana = 7
 Zgadywana = 0 
 
-while Zgadywana != szukana
-    Zgadywana = int(input("podaj liczbę: "))
+while Zgadywana != szukana:
+    Zgadywana = int(input("Podaj Liczbę: "))
+if Zgadywana < szukana:
+    print("Za mało")
+elif Zgadywana > szukana:
+    print("Za dużo")
 
-if Zgadywana == szukana
-    print("Brawo zgadłeś")
-elif
-    print("spróbuj jeszcze raz")
+print("Brawo to jest szukana wartość!")
