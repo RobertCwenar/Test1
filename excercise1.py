75a821f84d2f8a8bf2e299c42ffd263bd890a82e
Robert <110719811+RobertCwenar@users.noreply.github.com>
2025-11-17 10:40:21 +0000

    look for the number

--- excercise1.py
+++ excercise1.py
@@ -4,9 +4,9 @@ Zgadywana = 0
 
 while Zgadywana != szukana:
     Zgadywana = int(input("Podaj Liczbę: "))
-if Zgadywana < szukana:
-    print("Za mało")
-elif Zgadywana > szukana:
-    print("Za dużo")
+    if Zgadywana < szukana:
+        print("Za mało")
+    elif Zgadywana > szukana:
+        print("Za dużo")
 
 print("Brawo to jest szukana wartość!")
