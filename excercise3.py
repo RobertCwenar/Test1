5ae899aae2e7365204c0ce57485a61c1904033ba
Robert <110719811+RobertCwenar@users.noreply.github.com>
2025-11-20 07:22:31 +0000

    add comments in code

--- excercise3.py
+++ excercise3.py
@@ -1,3 +1,5 @@
+#exercise3.py
+#Create 3 variables for each guest: name, age, gender
 imie = "Arkadiusz"
 wiek = 29 
 plec = "Mężczyzna"
@@ -14,12 +16,14 @@ osoba1 = ('Arkadiusz', 29, 'Mężczyzna')
 osoba2 = ('Robert', 28, 'Mężczyzna')
 osoba3 = ('Kasia', 23, 'Kobieta')
 
+
+#crete listaGoscie with 3 guests
 listaGoscie = {
             ('Arkadiusz', 29, 'Mężczyzna'), 
             ('Robert', 28, 'Mężczyzna'), 
             ('Kasia', 23, 'Kobieta')}
 
-
+#crete listaGoscie2 with 3 new guests
 listaGoscie2 = {
             ('Wojtek', 15, 'Mężczyzna'), 
             ('Franek', 11, 'Mężczyzna'), 
@@ -28,7 +32,10 @@ listaGoscie2 = {
 #listaGoscie.append(['Dawid', 40, 'Mężczyzna'])
 #listaGoscie.append(['Dominika', 20, 'Kobieta'])
 
+#merge listaGoscie and listaGoscie2 into listaGoscie3
 listaGoscie3 = listaGoscie | listaGoscie2
 
+
+#loop for listaGoscie3 and print each element
 for imie, wiek, plec in listaGoscie3:
     print(imie, wiek, plec)
