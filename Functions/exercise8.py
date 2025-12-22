6debf58c5772e7c3bf15562b3bbb41fca0196ac8
Robert <110719811+RobertCwenar@users.noreply.github.com>
2025-12-22 13:47:36 +0000

    created folders

---
+++ Functions/exercise8.py
@@ -0,0 +1,14 @@
+def messege(name):
+    print (f"Hello, {name}! Welcome to the my programming world!")
+
+
+list = ["Arkadiuszu", "Jadziu", "Aleksandrze"]
+
+for name in list:
+    messege(name)
+
+
+# example definition:
+
+def area_of_squqare(side):
+    
