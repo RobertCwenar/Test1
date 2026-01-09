0517015e0c6eae6153842dbd138f1e04018d2bc1
Robert <110719811+RobertCwenar@users.noreply.github.com>
2026-01-09 10:59:04 +0000

    Add weapons and prompt player to continue each round

--- Random/my_game.py
+++ Random/my_game.py
@@ -38,8 +38,8 @@ Opponent's HP ≤ 0'''
 import random
 
 # HP gamer
-player_hp = 100
-enemy_hp = 100
+player_hp = 500
+enemy_hp = 500
 
 # Damage gamer 
 
@@ -49,12 +49,28 @@ enemy_damage = (7, 20)
 player_hit_chance = 70
 enemy_hit_chance = 60
 
+add_weapons = {"Weapon Bow": (30, 60),
+             "Weapon Sword": (25, 75),
+             "Weapon Degger": (20, 50),
+             "Weapon AX": (15, 35),
+            "Weapon magic staff": (20, 25)}
+
+def attack(player_base_damage, weapon_range, hit_chance):
+        if random.randint(1,100) <= hit_chance:
+            base_dmg = random.randint(*player_base_damage)
+            weapon_dmg = random.randint(*weapon_range)
+            total_dmg = base_dmg + weapon_dmg
+            return total_dmg
+        else:
+            return 0
+                          
 round_number = 1
+
 
 # Core Game
 
 while player_hp > 0 and enemy_hp > 0:
-    print(f"you survive we go to the next round.Round: {round_number}")
+    print(f"You survived we went to the next round. Round: {round_number}")
 
 # Player Attack
 
@@ -64,34 +80,49 @@ while player_hp > 0 and enemy_hp > 0:
         print(f"Player hit: {dmg}")
     else:
         print(f"Player Miss. Your hit 0 damage.")
-     
 
     if enemy_hp <= 0:
         print("Enemy is death.You win.")
         break
 
+    chosen_weapons = random.choice(list(add_weapons.keys()))
+    weapon_range = (add_weapons[chosen_weapons])
+    dmg = attack(player_damage, weapon_range, player_hit_chance)
+    enemy_hp -= dmg
+    print(f"Player hits with {chosen_weapons} for {dmg} damage")
+
 # Enemy attack 
     if random.randint(1, 100) <= enemy_hit_chance:
         dmg = random.randint(*enemy_damage)
         player_hp -= dmg 
-        print(f"Enemy hit: {dmg}")
+        print(f"Enemy hit: {dmg} and {chosen_weapons}")
     else:
         print(f"Enemy Miss. Your hit 0 damage.")
+    
+    chosen_weapons = random.choice(list(add_weapons.keys()))
+    weapon_range = (add_weapons[chosen_weapons])
+    dmg = attack(enemy_damage, weapon_range, enemy_hit_chance)
+    player_hp -= dmg
+    print(f"Enemy hits with {chosen_weapons} for {dmg} damage")
 
     if player_hp <= 0:
-        print("Player is death.You win.")
+        print("You lose.")
         break     
-
 
     round_number = round_number + 1
     
 
 # Output fight
-    print(f"Player attack: {player_damage}")
     print(f"Player HP: {player_hp}")
-    print(f"Enemey attack: {enemy_damage}")
     print(f"Enemy HP: {enemy_hp}")
 
+    # Ask player if they want to continue
+    choice = input("Continue to next round? (y/n): ").lower()
+    if choice != 'y': 
+        print("Game stopped player escape.")
+        break
+
+print("Game over")
 
 
 
